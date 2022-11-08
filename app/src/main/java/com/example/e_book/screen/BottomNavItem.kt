042a3a1419138f6d182259f494132669dfdce00c
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-11-08 16:26:01 +0530

    BottomNavItem

---
+++ app/src/main/java/com/example/e_book/screen/BottomNavItem.kt
@@ -0,0 +1,8 @@
+package com.example.e_book.screen
+
+import com.example.e_book.R
+//
+//open class BottomNavItem(s: String, icon: Int, route: Any) {
+//    object HomeScreen:BottomNavItem("Home", R.drawable.home, "Home")
+//
+//}
