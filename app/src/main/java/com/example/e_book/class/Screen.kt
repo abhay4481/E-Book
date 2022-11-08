f4d44a891934ba3b96055eca55dfce4d1ae762b4
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-11-08 16:23:16 +0530

    data class

--- app/src/main/java/com/example/e_book/class/Screen.kt
+++ app/src/main/java/com/example/e_book/class/Screen.kt
@@ -1,6 +1,24 @@
 package com.example.e_book.`class`
 
+import com.example.e_book.R
+
 sealed class Screens(val route: String) {
     object Splash : Screens("splash_screen")
-    object Home : Screens("home_screen")
+}
+sealed class Screen() {
+
+    object Profile:Screen()
+    object About:Screen()
+    object Setting:Screen()
+    object MoreBook:Screen()
+
+
+}
+sealed class BottomNavItem(var title:String, var icon:Int, var screen_route:String){
+
+    object Home : BottomNavItem("Home", R.drawable.home,"home")
+    object Favorite: BottomNavItem("Favorite",R.drawable.favorite,"Favorite")
+    object Audio: BottomNavItem("Audio",R.drawable.audio,"Audio")
+    object Download: BottomNavItem("Download",R.drawable.download,"Download")
+    object Setting: BottomNavItem("Setting",R.drawable.settings,"Setting")
 }
