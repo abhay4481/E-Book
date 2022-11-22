e15629eccc0d87a719338dc6ad900d5aa298dd78
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-11-23 00:03:20 +0530

    add some new color

--- app/src/main/java/com/example/e_book/ui/theme/Color.kt
+++ app/src/main/java/com/example/e_book/ui/theme/Color.kt
@@ -4,5 +4,5 @@ import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0XFF03DAC5)
-val Purple700 = Color(0xFF3700B3)
+val Purple700 = Color(0xFFFFF59D)
 val Teal200 = Color(0xFF03DAC5)
