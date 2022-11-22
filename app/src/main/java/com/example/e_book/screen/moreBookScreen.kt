d113910823ca9644abc6b3669a7c161a7b5e74a5
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-11-23 00:07:36 +0530

    more book screen

--- app/src/main/java/com/example/e_book/screen/moreBookScreen.kt
+++ app/src/main/java/com/example/e_book/screen/moreBookScreen.kt
@@ -25,6 +25,7 @@ import com.example.e_book.`class`.BottomNavItem
 import com.example.e_book.`class`.Screen
 import com.example.e_book.`class`.firebaseDataFile
 import com.example.e_book.data.dataProvider
+import com.example.e_book.data.dataProviderToCard
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -40,7 +41,7 @@ class MainViewModel   {
     private fun FetchDataFromDatabase(){
 
 
-        FirebaseDatabase.getInstance().getReference().child("fiction")
+        FirebaseDatabase.getInstance().reference.child("fiction")
             .addValueEventListener(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot){
                     for(DataSnap in snapshot.children) {
@@ -49,11 +50,8 @@ class MainViewModel   {
                         if (Song != null) {
                             temList.add(Song)
                         }
-
                     }
                 }
-
-
                 override fun onCancelled(error: DatabaseError) {
                     Log.w(ContentValues.TAG, "loadPost:onCancelled")
                 }
@@ -62,33 +60,37 @@ class MainViewModel   {
     }}
 val arr=MainViewModel()
 @Composable
-fun MoreBookScreen(typeofBook:dataProvider, navController: NavController){
-    Column( Modifier.padding(vertical = 20.dp)) {
-        Row(Modifier.padding(vertical = 6.dp, horizontal = 10.dp)) {
-            Image(
-                painter = painterResource(id =R.drawable.prev ),
-                contentDescription = null,
-                Modifier
-                    .size(50.dp)
-                    .padding(vertical = 10.dp)
-                    .clickable(onClick = { navController.navigate(BottomNavItem.Home.screen_route) })
-            )
-            Text(
-                text = typeofBook.bookName,
-                modifier = Modifier.padding(vertical = 10.dp, horizontal = 30.dp),
-                style = MaterialTheme.typography.h5
-            )
-        }
-        BookCard(name = arr.temList, navController)
-//        Bookshelf(arr.temList)
-    }
+fun MoreBookScreen(typeofBook:dataProvider, navController: NavController) {
+    Box() {
+        Image(painter = painterResource(id = R.drawable.back2), contentDescription = null, contentScale = ContentScale.FillBounds, modifier = Modifier.size(height = 1000.dp, width = 400.dp))
 
+        Column(Modifier.padding(vertical = 20.dp)) {
+            Row(Modifier.padding(vertical = 6.dp, horizontal = 10.dp)) {
+                Image(
+                    painter = painterResource(id = R.drawable.pre),
+                    contentDescription = null,
+                    Modifier
+                        .size(50.dp)
+                        .padding(vertical = 10.dp)
+                        .clickable(onClick = { navController.navigate(BottomNavItem.Home.screen_route) })
+                )
+                Text(
+                    text = typeofBook.bookName,
+                    modifier = Modifier.padding(vertical = 10.dp, horizontal = 30.dp),
+                    style = MaterialTheme.typography.h5,
+                    color = Color.White
+                )
+            }
+            BookCard(name = typeofBook.array, navController)
+//        Bookshelf(arr.temList)
+        }
+
+    }
 }
 
-
 @Composable
-fun BookCard(name: MutableList<firebaseDataFile>, navController: NavController ){
-    fun navigateToSingerPlaySong(singer: firebaseDataFile){
+fun BookCard(name: List<dataProviderToCard>, navController: NavController ){
+    fun navigateToSingerPlaySong(singer: dataProviderToCard){
         val PlayBook= Gson().toJson(singer)
         navController.navigate("Player/$PlayBook")
     }
@@ -105,12 +107,13 @@ fun BookCard(name: MutableList<firebaseDataFile>, navController: NavController )
     }
 }
 @Composable
-fun AlingmentBook(name: firebaseDataFile, navController: NavController, modifier: Modifier) {
-    Card(Modifier
+fun AlingmentBook(name: dataProviderToCard, navController: NavController, modifier: Modifier) {
+    Card(modifier
         .padding(vertical = 8.dp, horizontal = 17.dp)
         .height(190.dp)
-        .width(30.dp).clickable { navController.navigate(Screen.MoreBook.toString()) } , elevation = 4.dp , backgroundColor = Color.LightGray) {
-        Image(painter = rememberAsyncImagePainter(model = name.Image), contentDescription ="Image" , contentScale = ContentScale.FillBounds, modifier = Modifier
+        .width(30.dp)
+         , elevation = 4.dp , backgroundColor = Color.LightGray) {
+        Image(painter = rememberAsyncImagePainter(model = name.image), contentDescription ="Image" , contentScale = ContentScale.FillBounds, modifier = Modifier
             .height(150.dp)
             .width(50.dp)
         )
