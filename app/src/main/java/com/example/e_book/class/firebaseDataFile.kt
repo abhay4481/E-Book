7eef18e3e085bf384c79b308c47138c02abb19a2
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-11-23 00:06:27 +0530

    firebase datafile

--- app/src/main/java/com/example/e_book/class/firebaseDataFile.kt
+++ app/src/main/java/com/example/e_book/class/firebaseDataFile.kt
@@ -1,3 +1,4 @@
 package com.example.e_book.`class`
 
-data class firebaseDataFile( val Image:String="",val name:String="",val pdf:String="")
+data class firebaseDataFile( val name:String="",val Image:String="",val pdf:String="")
+
